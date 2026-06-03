221ab444100c3a6734d6b59fd29c7ea43f5ceff1
EJMILLER2205 <162735295+EJMILLER2205@users.noreply.github.com>
2026-06-03 18:07:43 -0400

    Update ppu.h

--- ppu.h
+++ ppu.h
@@ -14,12 +14,14 @@ public:
 private:
 	Bus* bus;
 	int cycleCount = 0; // Cycles accumulated on current scanline
+	int windowLine = 0; // tracks which window line we're on
 	bool frameComplete = false;
 
 	// Helper functions
 	void renderScanline(); // Renders current LY scanline
 	void renderBackground(); // Draws background tiles
 	void renderSprites(); // Draws sprites
+	void renderWindow(); // Renders window
 	uint8_t getLY() { return bus->read(0xFF44); }
 	void setLY(uint8_t v) { bus->write(0xFF44, v); }
 	uint8_t getLCDC() { return bus->read(0xFF40); }
@@ -27,4 +29,12 @@ private:
 	uint8_t getSCX() { return bus->read(0xFF43); }
 	uint8_t getBGP() { return bus->read(0xFF47); }
 	void setMode(uint8_t mode);
+
+	// Sprite reading helper functions
+	uint8_t getOBP0() { return bus->read(0xFF48); }
+	uint8_t getOBP1() { return bus->read(0xFF49); }
+
+	// Window reading helper functions
+	uint8_t getWY() { return bus->read(0xFF4A); }
+	uint8_t getWX() { return bus->read(0xFF4B); }
 };
